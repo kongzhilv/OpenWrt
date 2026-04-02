4bce674e083c01c35046e88bb4df8f9fc0f8f4c3
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-04-02 23:18:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# 描述: 编译前执行，用于修改系统默认配置
+# 描述: 编译前执行，用于修改系统默认配置和修复冲突
 
 # 1. 修改默认后台 IP 为 192.168.2.1 (防止和光猫冲突)
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 2. 动态修复第三方源 (small/kenzo) 中 homeproxy 和 momo 的死循环依赖报错
-# 强行删除 Makefile 中导致死循环的错误依赖声明
-find feeds/small/ -name "Makefile" -exec sed -i 's/+PACKAGE_sing-box-tiny//g' {} +
-find feeds/small/ -name "Makefile" -exec sed -i 's/+PACKAGE_luci-app-momo//g' {} +
-find feeds/kenzo/ -name "Makefile" -exec sed -i 's/+PACKAGE_sing-box-tiny//g' {} +
-find feeds/kenzo/ -name "Makefile" -exec sed -i 's/+PACKAGE_luci-app-momo//g' {} +
+# 2. 物理删除 kenzok8/small 源中引发“死循环”报错的无用插件
+# 这样在 make defconfig 扫描依赖时，就不会再报错崩溃了
+rm -rf feeds/small/luci-app-homeproxy
+rm -rf feeds/small/luci-app-momo
+rm -rf feeds/kenzo/luci-app-homeproxy
+rm -rf feeds/kenzo/luci-app-momo
