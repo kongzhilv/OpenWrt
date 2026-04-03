1241a3e2fab144c4da3c57e288fc4eb190ebb8ac
kongzhilv <157718507+kongzhilv@users.noreply.github.com>
2026-04-03 12:47:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 # 描述: 编译前执行，用于修改系统默认配置和修复冲突
 
-# 1. 修改默认后台 IP 为 192.168.2.1 (防止和光猫冲突)
+# 1. 修改默认后台 IP 为 192.168.2.1
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 2. 物理删除 kenzok8/small 源中引发“死循环”报错的无用插件
-# 这样在 make defconfig 扫描依赖时，就不会再报错崩溃了
+# 2. 物理删除 kenzok8/small 源中引发“死循环”报错的无用插件，防止编译失败
 rm -rf feeds/small/luci-app-homeproxy
 rm -rf feeds/small/luci-app-momo
 rm -rf feeds/kenzo/luci-app-homeproxy
 rm -rf feeds/kenzo/luci-app-momo
+
+# 3. 单独拉取最新的 Argon 主题源码
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+# 4. 强制将默认主题由 bootstrap 替换为 argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
